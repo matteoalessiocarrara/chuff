f186151c2a49272c0892b55c57e957945ed1d434
Matteo Alessio Carrara <sw.matteoac@gmail.com>
2017-11-22 19:13:23 +0100

    Refactoring, should work but not tested

--- main.c
+++ main.c
@@ -3,64 +3,54 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <stdlib.h>
-#include <assert.h>
-#include <stdint.h>
 
 #define die(...) {fprintf(stderr, __VA_ARGS__); exit(EXIT_FAILURE);}
+#define dbg(...) fprintf(stderr, __VA_ARGS__)
 
 struct node
 {
-	uint32_t freq;
-	uint8_t data;
+	unsigned int freq;
+	unsigned char data;
 	struct node *lc, *rc, *parent;
 };
 
-void encode(char *fn)
+int main(int argc, char **argv)
 {
 	FILE *fp;
-	uint8_t nc = 0, buf = 0, s = 0;
-	uint16_t sym = 0, qi=0;
-	uint32_t freq[256];
-	struct node *leaf, **wq, *nd = NULL, *bp[256], *tp;
+	unsigned char buf = 0, s = 0;
+	unsigned short nc = 0, syms = 0, qi=0;
+	static unsigned int freq[256];
+	static struct node leaf[256], *wq[256], nd[255], *lp[256], *tp;
 
-	if(!(fp = fopen(fn, "r"))) die("Cannot open file '%s'\n", fn);
+	if(argc < 2) die("Please give me a filename!\n");
+	if(!(fp = fopen(argv[1], "r"))) die("Cannot open input file\n");
 
-	// calc frequencies
-	for(short i = 0; i < 256; i++) freq[i] = 0;
-	for(int tmp; (tmp = fgetc(fp)) != EOF; freq[tmp]++);
-	
-	// calc used symbols
-	for(short i = 0; i < 256; i++) if(freq[i]) sym++;
-	if(!sym) die("There is nothing to do...\n");
-	
-	// initializes the tree structure
-	leaf = calloc(sym, sizeof(struct node));
-	wq = malloc(sizeof(struct node*) * sym);
-	for(short i = 0; i < 256; i++) 
+	// calc frequencies and used symbols
+	for(int c; (c = fgetc(fp)) != EOF; freq[c]++) if(!freq[c]) syms++;
+	if(!syms) die("There is nothing to do...\n");
+
+	// create leaf nodes and initialize working queue
+	for(short i = 0; i < 256; i++)
 	{
-		bp[i] = NULL;
 		if(freq[i])
 		{
 			leaf[qi] = (struct node){freq[i], i, NULL, NULL, NULL};
-			bp[i] = leaf + qi;
-			wq[qi] = leaf + qi;
+			wq[qi] = lp[i] = leaf + qi;
 			qi++;
 		}
 	}
-	
+
 	while(qi > 2)
 	{
 		// find the two smallest item
-		uint32_t f1 = wq[0]->freq, f2 = wq[1]->freq;
-		uint16_t idx1 = 0, idx2 = 1;
-		for(int i = 0; i < qi;  i++) if((wq[i]->freq < f1) && (i != idx2)) f1 = wq[idx1 = i]->freq;
-		for(int i = 0; i < qi;  i++) if((wq[i]->freq < f2) && (i != idx1)) f2 = wq[idx2 = i]->freq;
+		unsigned short idx1 = 0, idx2 = 1;
+		for(int i = 0; i < qi;  i++) if((wq[i]->freq < wq[idx1]->freq) && (i != idx2)) idx1 = i;
+		for(int i = 0; i < qi;  i++) if((wq[i]->freq < wq[idx2]->freq) && (i != idx1)) idx2 = i;
 
 		// create a parent node with them as child
-		nd = realloc(nd, sizeof(struct node) * ++nc);
-		nd[nc - 1] = (struct node){f1 + f2, 0, wq[idx1], wq[idx2], NULL};
-		wq[idx1]->parent = wq[idx2]->parent = nd + (nc - 1);
-		
+		nd[nc] = (struct node){wq[idx1]->freq + wq[idx2]->freq, 0, wq[idx1], wq[idx2], NULL};
+		wq[idx1]->parent = wq[idx2]->parent = nd + nc++;
+
 		// remove child from working queue and insert the parent
 		for(int i = (idx1 > idx2? idx1 : idx2); i < (qi - 1); i++) wq[i] = wq[i+1];
 		for(int i = (idx1 > idx2? idx2 : idx1); i < (qi - 1); i++) wq[i] = wq[i+1];
@@ -68,48 +58,41 @@ void encode(char *fn)
 	}
 
 	// create root node
-	nd = realloc(nd, sizeof(struct node) * ++nc);
-	nd[nc - 1] = (struct node){wq[0]->freq + wq[1]->freq, 0, wq[0], wq[1], NULL};
-	wq[0]->parent = wq[1]->parent = nd + (nc - 1);
-
+	nd[nc] = (struct node){wq[0]->freq + wq[1]->freq, 0, wq[0], wq[1], NULL};
+	wq[0]->parent = wq[1]->parent = nd + nc++;
 
 	// start encoding of the file
-	rewind(fp);
+
+	rewind(fp); // this will not work if the input is taken from stdin!
+	// for complexity reasons, the file will be wrote from right to left
 	for(int tmp; (tmp = fgetc(fp)) != EOF;)
 	{
 		// if we can encode this
-		if((tp = bp[tmp]))
+		if((tp = lp[tmp]))
 		{
 			// get the path until we arrive to the root node
+			// and write it into the output buffer
 			do
 			{
-				if(s > 7)
+				if(s > 7) // buffer is full
 				{
-					printf("%c", buf);
+					printf("%uc", buf);
 					buf = s = 0;
 				}
-				if(tp->parent->lc == tp)
+				if(tp == tp->parent->lc)
 				{
 					buf &= ~(1 << s++);
 				}
-				else if(tp->parent->rc == tp)
+				else if(tp == tp->parent->rc)
 				{
 					buf |= (1 << s++);
 				}
-				else die("Invalid node references\n");
+				else die("Invalid node reference\n");
 				tp = tp->parent;
 			}while(tp->parent);
 		}
-		else die("Cannot encode symbol %x! File may have been changed...\n", tmp);
+		else die("Cannot encode symbol %x! Input may have been changed...\n", tmp);
 	}
-	//flush the buffer
-	printf("%c", buf);
-
-	fclose(fp);
-}
-
-int main(int argc, char **argv)
-{
-	if(argc < 2) die("Usage: hf filename\n");
-	encode(argv[1]);
+	printf("%uc%uc", buf, 8 - s); // flush the buffer and writes the number of padding bits in the last byte
+	return 0;
 }
